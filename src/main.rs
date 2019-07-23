5ad1bce03ff9a974d614e9a87640b12ca6a31b25
soupersauce <soupersauce@gmail.com>
2019-07-23 15:59:16 -0700

    Starting bare ggez context

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,48 @@ extern crate serde_derive;
 use tcod::console::*;
 use tcod::map::Map as FovMap;
 
-use ggez::{self, *};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
 
 mod game;
 use crate::game::*;
+
+struct Game {
+
+}
+
+impl Game {
+    pub fn new(_ctx: &mut Context) -> Game {
+        // Load/Create resources such as images here.
+        Game {
+            // TODO
+        }
+    }
+}
+
+impl EventHandler for Game {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Code here
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        //draw code here
+        graphics::present(ctx)
+    }
+}
 
 fn main() {
     let (mut ctx, mut event_loop) = ContextBuilder::new("Rugue", "sauceCo")
         .build()
         .expect("Error: Could not create context");
 
-    let game = Game::new(&mut ctx);
+    let mut game = Game::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occurred: {}", e),
+    }
 }
